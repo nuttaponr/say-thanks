46688826d851b6a42e80fa0368c3a835d1c59911
hei_hei_hei <53534132+nuttaponr@users.noreply.github.com>
2026-07-05 18:34:48 +0700

    Update version number to 1.0.1.15

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bearbit+
 // @namespace    Violentmonkey Scripts
-// @version      1.0.1.14
+// @version      1.0.1.15
 // @description  Auto "say thanks" Bearbit
 // @author       You
 // @match        *://*.bearbit.org/*
